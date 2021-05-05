4f8645eb46825091d80252a489d4646f6f25fa1f
Daegyeom Song <LeoSong7077@gmail.com>
2021-05-05 11:16:59 +0900

    모든문제

--- 유형별 연습문제/가장 큰 수.js
+++ 유형별 연습문제/가장 큰 수.js
@@ -1,3 +1,5 @@
+//X
+
 // 정수 타입
 // [6, 10, 2] -> "6210"
 // [3, 30, 34, 5, 9] -> "9534330"
@@ -10,8 +12,35 @@
     //가장 큰 자리수에 맞춰서 같은 숫자로 채워 넣어서 정렬한다.
 
 function solution(numbers) {
-    var answer = [number.length];
-    return answer;
+    var answer;
+
+    let arr = numbers;
+    arr.sort();
+
+    let result = [];
+    let tempArr = [arr[arr.length - 1].toString()]; //'-1'
+    for (let i = arr.length - 2; i >= 0; i--) {
+        let firstNumber = arr[i].toString()[0];
+        if (tempArr[tempArr.length - 1][0] !== firstNumber) { //달라지는 순간이 오면
+            if (tempArr.length === 1) { //하나면 그냥 추가
+                result.push(tempArr[0]);
+            }
+            else {
+                result = result.concat(tempArr.sort()); //여러개면 정렬 후 추가
+            }
+            tempArr = [];
+        }
+        tempArr.push(arr[i].toString());
+    }
+    if (tempArr.length === 1) {
+        result.push(tempArr[0]);
+    }
+    else {
+        result = result.concat(tempArr.sort());
+    }
+    answer = result.join('');
+
+    return result;
 }
 
 //console.log([3, 300, 5 , 30, 9].sort((a, b) => {return a.toString() - b.toString();})); //보니까 그냥 기본 해시코드 정렬로 해도 괜찮다.. 
@@ -22,30 +51,8 @@ function solution(numbers) {
 //그 숫자와 다음 숫자 갚이 같으면 작업 배열에 계속 저장.
 //다음 숫자와 값이 달라졌을때, 정렬하고 최종결과 배열에 더하고, 다시 작업 배열을 새롭게 다시 채운다..
 
-let arr = [3, 30, 34, 5, 9];
-arr.sort();
-
-let result = [];
-let tempArr = [arr[arr.length - 1].toString()]; //'-1'
-for (let i = arr.length - 2; i >= 0; i--) {
-    let firstNumber = arr[i].toString()[0];
-    if (tempArr[tempArr.length - 1][0] !== firstNumber) { //달라지는 순간이 오면
-        if (tempArr.length === 1) {
-            result.push(tempArr[0]);
-        }
-        else {
-            result = result.concat(tempArr.sort());
-        }
-        tempArr = [];
-    }
-    tempArr.push(arr[i].toString());
-}
-if (tempArr.length === 1) {
-    result.push(tempArr[0]);
-}
-else {
-    result = result.concat(tempArr.sort());
-}
 
 
-console.log(result);
+
+//console.log(solution([3, 30, 34, 5, 9]));
+console.log([3,34,30].sort());
